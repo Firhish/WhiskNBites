ce83cc7d2f3db7089e6d5582051763406c575de9
Firhish <firdaushishamudin238@gmail.com>
2023-03-21 08:56:39 +0800

    Test 2

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ function App(): JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
+          <Section title="testing from main branch">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
