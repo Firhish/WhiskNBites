84d6903fefd8778c0a5ea92b0bc8e46fdca366c8
Firhish <firdaushishamudin238@gmail.com>
2023-04-12 17:15:39 +0800

    Fixed UI for option/confirm modal

--- components/OptionModal.js
+++ components/OptionModal.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import { Modal, Text, TouchableHighlight, View, Dimensions } from 'react-native';
+import { Modal, Text, TouchableHighlight, View, Dimensions, StyleSheet } from 'react-native';
 const { width } = Dimensions.get('window');
 
 class OptionModal extends Component {
   
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     modalVisible: false,
-  //   };
-  // }
-
-  // setModalVisible(visible) {
-  //   this.setState({ modalVisible: visible });
-  // }
-
   render() {
     return (
 
@@ -25,42 +14,35 @@ class OptionModal extends Component {
         onRequestClose={() => {
           window.alert('Modal has been closed.');
         }}>
-        <View style={{ marginTop:'55%', backgroundColor: '#fff', padding: 25,margin:'5%',borderRadius:10 }}>
-          <Text style={{ marginBottom: 20 }}>Please choose an option:</Text>
+        <View style={styles.mainContainer}>
+          <Text style={styles.title}>Please choose an option</Text>
           <TouchableHighlight
-            underlayColor={'#DB9B06c6'}
-            style={{ padding: 10, backgroundColor: '#eee', marginBottom: 10 }}
+            underlayColor={'transparent'}
+            style={styles.firstBtn}
             onPress={() => {
-              // this.setModalVisible(!this.state.modalVisible);
               this.props.toggle()
               this.props.firstOptFunc()
-              // console.log('Delete Product')
-              // Handle first option
             }}>
-            <Text>{this.props.firstOptText}</Text>
+            <Text style={styles.firstBtnText}>{this.props.firstOptText}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight
-            underlayColor={'#DB9B06c6'}
-            style={{ padding: 10, backgroundColor: '#eee' }}
+            underlayColor={'transparent'}
+            style={styles.secondBtn}
             onPress={() => {
-              // this.setModalVisible(!this.state.modalVisible);
               this.props.toggle()
               this.props.secondOptFunc()
-              // console.log('Edit Product')
-              // Handle second option
             }}>
-            <Text>{this.props.secondOptText}</Text>
+            <Text style={styles.secondBtnText}>{this.props.secondOptText}</Text>
           </TouchableHighlight>
 
           <TouchableHighlight
-            underlayColor={'#DB9B06c6'}
-            style={{ padding: 10, backgroundColor: '#ccc', marginTop: 10 }}
+            underlayColor={'transparent'}
+            style={styles.cancelBtn}
             onPress={() => {
-              // this.setModalVisible(!this.state.modalVisible);
               this.props.toggle()
             }}>
-            <Text>Cancel</Text>
+            <Text style={styles.cancelBtnText}>Cancel</Text>
           </TouchableHighlight>
         </View>
       </Modal>
@@ -69,5 +51,92 @@ class OptionModal extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+  mainContainer: { 
+    marginTop: '55%', 
+    backgroundColor: '#fff', 
+    padding: 25, 
+    margin: '5%', 
+    borderRadius: 10, 
+  },
+
+  title:{ 
+    marginBottom: 20, 
+    textAlign:'center',
+    fontWeight: 'bold',
+    fontSize: 18, 
+  },
+
+  firstBtn:{ 
+    textAlign:'center',
+    marginBottom: 10, 
+    // padding: 10, 
+    // backgroundColor: 'yellow',
+     
+  },
+
+  firstBtnText:{
+    textAlign:'center',
+    borderRadius:10, 
+    // borderWidth:2,
+    paddingVertical:10,
+    backgroundColor:'#DB9B06',
+    color:'white',
+    fontSize:16,
+  },
+
+  secondBtn:{ 
+    // padding: 10, 
+    // backgroundColor: 'yellow',
+    marginBottom: 20, 
+    
+  },
+
+  secondBtnText:{
+    width:'100%',
+    textAlign:'center',
+    borderRadius:10, 
+    // borderWidth:2,
+    paddingVertical:10,
+    backgroundColor:'#DB9B06',
+    color:'white',
+    fontSize:16,
+  },
+
+
+  cancelBtn:{ 
+    // padding: 10, 
+    // textAlign:'center',
+    // backgroundColor: 'yellow',
+  },
+
+  cancelBtnText:{
+    // width:'100%',
+    // textAlign:'center',
+    borderRadius:10, 
+    borderWidth:2,
+    borderColor: '#a0a0a0',
+    paddingVertical:10,
+    // backgroundColor:'#a0a0a0',
+    color:'#a0a0a0',
+    fontSize:16,
+    textAlign:'center',
+    fontWeight:'bold',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+})
 
 export default OptionModal;
